539ec18a65dd209de8abce43d276d5948ba6dfbe
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-26 17:13:10 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "project" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "sg" {
+  default = "sg-034aed3b2d243f095"
+}
